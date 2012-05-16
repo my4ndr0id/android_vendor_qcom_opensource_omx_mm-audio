8d4cfe6033d7f814b39fc4044f349faddff2a999
Harmandeep Singh <hsingh@codeaurora.org>
2012-05-15 19:04:20 -0700

    mm-audio: Add support for compiling hardware encoders for copper target.
    - Add support for compiling hardware encoders for 8974 copper target
    
    Change-Id: Iacb8f2506f0543b9ba45a75c3bb15258c1c0ce24

--- aenc-amrnb/Android.mk
+++ aenc-amrnb/Android.mk
@@ -9,5 +9,8 @@ endif
 ifeq ($(call is-board-platform,msm8960),true)
 include $(AENC_AMR_PATH)/qdsp6/Android.mk
 endif
+ifeq ($(call is-board-platform,copper),true)
+include $(AENC_AMR_PATH)/qdsp6/Android.mk
+endif
 
 endif
